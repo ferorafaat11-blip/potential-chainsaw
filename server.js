7eb5ab06170a3961335ed1253254782cb79b55e6
ferorafaat11-blip <ferorafaat11@gmail.com>
2026-03-16 05:53:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,175 +1,171 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
 app.use(express.static('public'));
 
-var state = {
-  type: 'video', url: '', degree: 0,
-  playing: false, startedAt: null, pausedAt: 0,
-  volume: 1, muted: false,
-  audioUrl: '', audioPlaying: false,
-  audioStartedAt: null, audioPausedAt: 0, audioVolume: 1
-};
-
+var media = { type:'video', url:'', degree:0, playing:false, startedAt:null, pausedAt:0, volume:1, muted:false };
+var audio = { url:'', playing:false, startedAt:null, pausedAt:0, volume:1 };
 var viewers = {};
-var counter = 0;
 var hosts = {};
-var allowJoin = false;
+var count = 0;
+var open = false;
 
-function bcast() {
+function broadcast() {
   var list = Object.values(viewers);
-  io.emit('viewers', list.length);
-  io.emit('viewerList', list);
+  io.emit('vlist', list);
+  io.emit('vcount', list.length);
 }
 
-function gs() {
-  return {
-    type: state.type, url: state.url, degree: state.degree,
-    playing: state.playing, startedAt: state.startedAt, pausedAt: state.pausedAt,
-    volume: state.volume, muted: state.muted, serverTime: Date.now()
-  };
-}
+io.on('connection', function(socket) {
 
-io.on('connection', function(s) {
-
-  s.on('host', function() {
-    hosts[s.id] = true;
-    if (viewers[s.id]) { delete viewers[s.id]; bcast(); }
+  socket.on('join-host', function() {
+    hosts[socket.id] = true;
+    delete viewers[socket.id];
+    broadcast();
   });
 
-  s.on('viewer', function(data) {
-    if (hosts[s.id]) return;
-    if (!viewers[s.id]) {
-      counter++;
-      viewers[s.id] = { id: s.id, num: counter, name: (data && data.name) || ('جهاز ' + counter), status: 'انتظار' };
-      bcast();
+  socket.on('join-viewer', function(data) {
+    if (hosts[socket.id]) return;
+    if (!viewers[socket.id]) {
+      count++;
+      viewers[socket.id] = {
+        id: socket.id,
+        num: count,
+        name: (data && data.name) ? data.name : 'جهاز ' + count,
+        status: 'انتظار'
+      };
+      broadcast();
     }
-    s.emit('joinOk', { allow: allowJoin });
-    s.emit('state', gs());
-    if (state.audioUrl) {
-      s.emit('audio', { playing: state.audioPlaying, url: state.audioUrl, startedAt: state.audioStartedAt, pausedAt: state.audioPausedAt, volume: state.audioVolume, serverTime: Date.now() });
-    }
+    socket.emit('welcome', { open: open, media: media, audio: audio, serverTime: Date.now() });
   });
 
-  s.on('open', function(data) {
-    hosts[s.id] = true;
-    if (viewers[s.id]) { delete viewers[s.id]; bcast(); }
-    allowJoin = data.allow;
+  socket.on('set-open', function(data) {
+    hosts[socket.id] = true;
+    delete viewers[socket.id];
+    open = data.open;
     var ids = Object.keys(viewers);
     for (var i = 0; i < ids.length; i++) {
-      var c = io.sockets.sockets.get(ids[i]);
-      if (allowJoin) {
-        if (c) c.emit('go');
+      var s = io.sockets.sockets.get(ids[i]);
+      if (open) {
+        if (s) s.emit('room-open');
         if (viewers[ids[i]]) viewers[ids[i]].status = 'اختيار وضع';
       } else {
-        if (c) c.emit('stop');
+        if (s) s.emit('room-closed');
         if (viewers[ids[i]]) viewers[ids[i]].status = 'انتظار';
       }
     }
-    bcast();
-    s.emit('openOk', { allow: allowJoin });
+    broadcast();
+    socket.emit('open-ok', { open: open });
   });
 
-  s.on('play', function(data) {
-    if (data.url) state.url = data.url;
-    if (data.type) state.type = data.type;
-    if (data.degree !== undefined) state.degree = data.degree;
-    if (data.volume !== undefined) state.volume = data.volume;
-    if (data.muted !== undefined) state.muted = data.muted;
-    state.playing = true;
-    // forceRestart: ابدأ من الصفر دايماً
-    if (data.forceRestart) {
-      state.pausedAt = 0;
-      state.startedAt = Date.now();
-    } else {
-      state.startedAt = Date.now() - (state.pausedAt * 1000);
-    }
-    io.emit('state', gs());
+  socket.on('cmd-play', function(data) {
+    media.url     = data.url     || media.url;
+    media.type    = data.type    || media.type;
+    media.degree  = data.degree  !== undefined ? data.degree  : media.degree;
+    media.volume  = data.volume  !== undefined ? data.volume  : media.volume;
+    media.muted   = data.muted   !== undefined ? data.muted   : media.muted;
+    media.playing   = true;
+    media.pausedAt  = 0;
+    media.startedAt = Date.now();
+    io.emit('media', Object.assign({}, media, { serverTime: Date.now() }));
   });
 
-  s.on('pause', function() {
-    if (state.startedAt) state.pausedAt = (Date.now() - state.startedAt) / 1000;
-    state.playing = false;
-    io.emit('state', gs());
+  socket.on('cmd-pause', function() {
+    if (media.startedAt) media.pausedAt = (Date.now() - media.startedAt) / 1000;
+    media.playing = false;
+    io.emit('media', Object.assign({}, media, { serverTime: Date.now() }));
   });
 
-  s.on('seek', function(data) {
-    state.pausedAt = data.t;
-    state.startedAt = Date.now() - (data.t * 1000);
-    io.emit('state', gs());
+  socket.on('cmd-seek', function(data) {
+    media.pausedAt  = data.t;
+    media.startedAt = Date.now() - (data.t * 1000);
+    io.emit('media', Object.assign({}, media, { serverTime: Date.now() }));
   });
 
-  s.on('vol', function(data) {
-    if (data.v !== undefined) state.volume = data.v;
-    if (data.m !== undefined) state.muted = data.m;
-    io.emit('vol', { v: state.volume, m: state.muted });
+  socket.on('cmd-vol', function(data) {
+    media.volume = data.v !== undefined ? data.v : media.volume;
+    media.muted  = data.m !== undefined ? data.m : media.muted;
+    io.emit('vol', { v: media.volume, m: media.muted });
   });
 
-  s.on('sync', function() { io.emit('state', gs()); });
-
-  s.on('dur', function(data) { io.emit('dur', data); });
-
-  s.on('adur', function(data) { io.emit('adur', data); });
-
-  s.on('aplay', function(data) {
-    if (data.url) state.audioUrl = data.url;
-    if (data.v !== undefined) state.audioVolume = data.v;
-    state.audioPlaying = true;
-    state.audioStartedAt = Date.now() - (state.audioPausedAt * 1000);
-    io.emit('audio', { playing: true, url: state.audioUrl, startedAt: state.audioStartedAt, pausedAt: state.audioPausedAt, volume: state.audioVolume, serverTime: Date.now() });
+  socket.on('cmd-sync', function() {
+    io.emit('media', Object.assign({}, media, { serverTime: Date.now() }));
   });
 
-  s.on('apause', function() {
-    if (state.audioStartedAt) state.audioPausedAt = (Date.now() - state.audioStartedAt) / 1000;
-    state.audioPlaying = false;
-    io.emit('audio', { playing: false, pausedAt: state.audioPausedAt, volume: state.audioVolume });
+  socket.on('cmd-dur', function(d) {
+    io.emit('dur', d);
   });
 
-  s.on('arestart', function(data) {
-    if (data && data.url) state.audioUrl = data.url;
-    if (data && data.v !== undefined) state.audioVolume = data.v;
-    state.audioPlaying = true; state.audioPausedAt = 0;
-    state.audioStartedAt = Date.now();
-    io.emit('audio', { playing: true, url: state.audioUrl, startedAt: state.audioStartedAt, pausedAt: 0, volume: state.audioVolume, serverTime: Date.now() });
+  socket.on('audio-play', function(data) {
+    audio.url       = data.url   || audio.url;
+    audio.volume    = data.v     !== undefined ? data.v : audio.volume;
+    audio.playing   = true;
+    audio.pausedAt  = 0;
+    audio.startedAt = Date.now();
+    io.emit('audio', Object.assign({}, audio, { serverTime: Date.now() }));
   });
 
-  s.on('astop', function() {
-    state.audioPlaying = false; state.audioPausedAt = 0; state.audioStartedAt = null;
+  socket.on('audio-pause', function() {
+    if (audio.startedAt) audio.pausedAt = (Date.now() - audio.startedAt) / 1000;
+    audio.playing = false;
+    io.emit('audio', Object.assign({}, audio, { serverTime: Date.now() }));
+  });
+
+  socket.on('audio-restart', function(data) {
+    audio.url       = data.url || audio.url;
+    audio.volume    = data.v   !== undefined ? data.v : audio.volume;
+    audio.playing   = true;
+    audio.pausedAt  = 0;
+    audio.startedAt = Date.now();
+    io.emit('audio', Object.assign({}, audio, { serverTime: Date.now() }));
+  });
+
+  socket.on('audio-stop', function() {
+    audio.playing   = false;
+    audio.pausedAt  = 0;
+    audio.startedAt = null;
     io.emit('audio', { playing: false, stop: true });
   });
 
-  s.on('aseek', function(data) {
-    state.audioPausedAt = data.t;
-    state.audioStartedAt = Date.now() - (data.t * 1000);
-    io.emit('audio', { playing: state.audioPlaying, url: state.audioUrl, startedAt: state.audioStartedAt, pausedAt: state.audioPausedAt, volume: state.audioVolume, serverTime: Date.now() });
+  socket.on('audio-seek', function(data) {
+    audio.pausedAt  = data.t;
+    audio.startedAt = Date.now() - (data.t * 1000);
+    io.emit('audio', Object.assign({}, audio, { serverTime: Date.now() }));
   });
 
-  s.on('avol', function(data) {
-    if (data.v !== undefined) state.audioVolume = data.v;
-    io.emit('avol', { v: state.audioVolume });
+  socket.on('audio-vol', function(data) {
+    audio.volume = data.v !== undefined ? data.v : audio.volume;
+    io.emit('avol', { v: audio.volume });
   });
 
-  s.on('kick', function(data) {
+  socket.on('audio-dur', function(d) {
+    io.emit('adur', d);
+  });
+
+  socket.on('kick-one', function(data) {
     var t = io.sockets.sockets.get(data.id);
-    if (t) { t.emit('kicked'); delete viewers[data.id]; bcast(); }
+    if (t) { t.emit('kicked'); delete viewers[data.id]; broadcast(); }
   });
 
-  s.on('kickall', function() { io.emit('kicked'); });
-
-  s.on('status', function(data) {
-    if (viewers[s.id]) { viewers[s.id].status = data.s; bcast(); }
+  socket.on('kick-all', function() {
+    io.emit('kicked');
   });
 
-  s.on('disconnect', function() {
-    delete viewers[s.id]; delete hosts[s.id]; bcast();
+  socket.on('viewer-status', function(data) {
+    if (viewers[socket.id]) { viewers[socket.id].status = data.s; broadcast(); }
+  });
+
+  socket.on('disconnect', function() {
+    delete viewers[socket.id];
+    delete hosts[socket.id];
+    broadcast();
   });
 
 });
 
 var PORT = process.env.PORT || 3000;
-server.listen(PORT, function() { console.log('port ' + PORT); });
+httpServer.listen(PORT, function() { console.log('listening on ' + PORT); });
